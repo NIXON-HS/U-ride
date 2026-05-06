1b5e5b24a58982105415761afd8214db39eedec0
Alan Cuenca <mateotkd123@gmail.com>
2026-05-06 15:30:40 -0500

    se quitaron datos sensibles

--- backend/createAdmin.js
+++ backend/createAdmin.js
@@ -4,11 +4,11 @@ const bcrypt = require('bcryptjs');
 async function seedAdmin() {
    try {
       console.log("Inyectando Super Administrador Secreto...");
-      const passwordCifrada = await bcrypt.hash('Alan2005_', 10);
+      const passwordCifrada = await bcrypt.hash('seguridad123', 10);
       
       const sql = `
          INSERT INTO usuarios (nombre, email, password_hash, rol) 
-         VALUES ('Alan Puruncajas', 'apuruncajas1832@uta.edu.ec', $1, 'ADMINISTRADOR')
+         VALUES ('Admin', 'admi@uta.edu.ec', $1, 'ADMINISTRADOR')
          ON CONFLICT (email) DO NOTHING
       `;
       
@@ -16,8 +16,8 @@ async function seedAdmin() {
       console.log("✅ Acceso garantizado a la base de datos.");
       console.log("------------------------------------------");
       console.log("Credenciales de la Plataforma Web:");
-      console.log("Usuario: apuruncajas1832@uta.edu.ec");
-      console.log("Password: Alan2005_");
+      console.log("Usuario: admi@uta.edu.ec");
+      console.log("Password: seguridad123");
       console.log("------------------------------------------");
    } catch(e) {
       console.log("Error creando admin:", e);
